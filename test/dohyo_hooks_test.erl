a2e133536dafbfb3f934133e9aed2b6cfc950fcd
Aaron Spiegel <spiegela@gmail.com>
2015-03-01 20:06:19 -0600

    cover hook execution

--- test/dohyo_hooks_test.erl
+++ test/dohyo_hooks_test.erl
@@ -51,24 +51,114 @@ unit_test_() ->
         fun unmock_hook_handler_setup/1,
         fun starts_and_registers_handler2/0
       }
+    },
+    { "runs on_create hook functions",
+      { setup,
+        fun mock_hook_handler_setup2/0,
+        fun unmock_hook_handler_setup/1,
+        fun runs_on_create/0
+      }
+    },
+    { "runs on_update hook functions",
+      { setup,
+        fun mock_hook_handler_setup2/0,
+        fun unmock_hook_handler_setup/1,
+        fun runs_on_update/0
+      }
+    },
+    { "runs on_delete hook functions",
+      { setup,
+        fun mock_hook_handler_setup2/0,
+        fun unmock_hook_handler_setup/1,
+        fun runs_on_delete/0
+      }
+    },
+    { "runs on_delete_all hook functions",
+      { setup,
+        fun mock_hook_handler_setup2/0,
+        fun unmock_hook_handler_setup/1,
+        fun runs_on_delete_all/0
+      }
+    },
+    { "runs on_schema_create hook functions",
+      { setup,
+        fun mock_hook_handler_setup2/0,
+        fun unmock_hook_handler_setup/1,
+        fun runs_on_schema_create/0
+      }
     }
   ].
 
 starts_and_registers_handler() ->
   ok = dohyo_hooks:start(),
-  ?assertEqual({ok, [fakepid]}, application:get_env(sumo_db, events)).
+  [ ?_assertEqual({ok, [fakepid]}, application:get_env(sumo_db, events)),
+    ?_assert(meck:validate(dohyo_hook_handler)),
+    ?_assert(meck:called(dohyo_hook_handler, start_link, [article])),
+    ?_assert(meck:validate(article)),
+    ?_assert(meck:called(article, schema , [])),
+    ?_assert(meck:validate(comment)),
+    ?_assert(meck:called(comment, schema , []))
+  ].
 
 starts_and_registers_handler2() ->
   ok = dohyo_hooks:start(),
-  ?assertEqual( {ok, [fakepid,some_hook_handler_pid]},
-                application:get_env(sumo_db, events)
-              ).
+  [ ?assertEqual( {ok, [fakepid,some_hook_handler_pid]},
+                  application:get_env(sumo_db, events)
+                ),
+    ?_assert(meck:validate(dohyo_hook_handler)),
+    ?_assert(meck:called(dohyo_hook_handler, start_link, [article])),
+    ?_assert(meck:validate(article)),
+    ?_assert(meck:called(article, schema , [])),
+    ?_assert(meck:validate(comment)),
+    ?_assert(meck:called(comment, schema , []))
+  ].
+
+runs_on_create() ->
+  ok = dohyo_hooks:on_create(article, []),
+  [ ?_assert(meck:validate(article)),
+    ?_assert(meck:called(article, schema , [])),
+    ?_assert(meck:called(fakemod, hook, ["fired"]))
+  ].
+
+runs_on_update() ->
+  ok = dohyo_hooks:on_update(article, []),
+  [ ?_assert(meck:validate(article)),
+    ?_assert(meck:called(article, schema , [])),
+    ?_assert(meck:called(fakemod, hook, ["fired"]))
+  ].
+
+runs_on_delete() ->
+  ok = dohyo_hooks:on_delete(article, 123),
+  [ ?_assert(meck:validate(article)),
+    ?_assert(meck:called(article, schema , [])),
+    ?_assert(meck:called(fakemod, hook, ["fired"]))
+  ].
+
+runs_on_delete_all() ->
+  ok = dohyo_hooks:on_delete_all(article),
+  [ ?_assert(meck:validate(article)),
+    ?_assert(meck:called(article, schema , [])),
+    ?_assert(meck:called(fakemod, hook, ["fired"]))
+  ].
+
+runs_on_schema_create() ->
+  ok = dohyo_hooks:on_schema_create(article),
+  [ ?_assert(meck:validate(article)),
+    ?_assert(meck:called(article, schema , [])),
+    ?_assert(meck:called(fakemod, hook, ["fired"]))
+  ].
 
 article_schema() ->
+  HookFun1 = fun(_) -> fakemod:hook("fired") end,
+  HookFun2 = fun() -> fakemod:hook("fired") end,
   [ #field{name = id, type = integer, attrs = [not_null, autoincrement]},
     #field{name = title, type = string},
     #field{name = content, type = text},
-    #hook{type = on_create, func = fun(_, _) -> io:format("fired") end}
+    #hook{type = on_create, func = HookFun1},
+    #hook{type = on_update, func = HookFun1},
+    #hook{type = on_delete, func = HookFun1},
+    #hook{type = on_delete_all, func = HookFun2},
+    #hook{type = on_schema_create, func = HookFun2}
   ].
 
 comment_schema() ->
@@ -82,6 +172,8 @@ mock_hook_handler_setup() ->
   meck:expect(article, schema, [], article_schema()),
   meck:new(comment, [non_strict]),
   meck:expect(comment, schema, [], comment_schema()),
+  meck:new(fakemod, [non_strict]),
+  meck:expect(fakemod, hook, ["fired"], ok),
   application:set_env(sumo_db, docs, [{article, mysql}, {comment, mysql}]).
 
 mock_hook_handler_setup2() ->
@@ -92,5 +184,6 @@ unmock_hook_handler_setup(_) ->
   meck:unload(dohyo_hook_handler),
   meck:unload(article),
   meck:unload(comment),
+  meck:unload(fakemod),
   application:unset_env(sumo_db, events),
   application:unset_env(sumo_db, docs).
